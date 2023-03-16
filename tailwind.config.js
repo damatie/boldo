121794eceecd988b7283ddab8670930870359054
damatie <edafemaxwell@gmail.com>
2023-03-16 15:37:01 +0100

    chore: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,39 @@ module.exports = {
       },
       dropShadow: {
         boldoSm :'0px 4px 32px rgba(0, 0, 0, 0.08)'
+      },keyframes: {
+        'zoom-in': {
+          '0%': {
+            transform: 'scale(.7)',
+          },
+          '100%': {
+            transform: 'scale(1)',
+          },
+        },
+        'fade-up': {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(10rem)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-in': {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        'zoom-in': 'zoom-in .3s ease-in-out',
+        'zoom-out': 'zoom-in .3s ease-in-out reverse',
+        'fade-up': 'fade-up .5s ease-in-out',
+        'fade-in': 'fade-in .5s ease-in-out',
       }
     },
   },
